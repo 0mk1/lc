4b83f0ffa7809f74948c59ddedd32f65a11e4aca
Mateusz Kamycki <mateusz.kamycki@gmail.com>
2018-10-29 08:53:07 +0100

    WIP

--- lc.go
+++ lc.go
@@ -1,14 +1,34 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"os"
 
 	"github.com/urfave/cli"
 )
 
+func parseArgs(c *cli.Context) {
+}
+
+func countLines() {
+}
+
+func countLinesInFile() {
+}
+
 func main() {
-	err := cli.NewApp().Run(os.Args)
+	app := cli.NewApp()
+	app.Name = "code line counter"
+	app.Usage = "lc <folder/file>"
+
+	app.Action = func(c *cli.Context) error {
+		// TODO: Add https://github.com/derekparker/delve to debug
+		fmt.Printf("Hello %q", c.Args().Get(0))
+		return nil
+	}
+
+	err := app.Run(os.Args)
 	if err != nil {
 		log.Fatal(err)
 	}

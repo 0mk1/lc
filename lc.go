fe3288cb0cfb5bc396f53cd86b2bd9c3b8668c13
Mateusz Kamycki <mateusz.kamycki@gmail.com>
2018-11-03 10:42:28 +0100

    Add handling folder / file as argument in CLI & Initial configs (#1)

---
+++ lc.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+
+	"github.com/urfave/cli"
+)
+
+func getFiles(folderPath string) []string {
+	fileList := make([]string, 0)
+	err := filepath.Walk(folderPath, func(path string, f os.FileInfo, err error) error {
+		if !f.IsDir() {
+			fileList = append(fileList, path)
+		}
+		return err
+	})
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	return fileList
+}
+
+func main() {
+	app := cli.NewApp()
+	app.Name = "code line counter"
+
+	app.Action = func(c *cli.Context) error {
+		path := c.Args().First()
+		fi, err := os.Stat(path)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		var files []string
+		switch mode := fi.Mode(); {
+		case mode.IsDir():
+			files = append(files, getFiles(path)...)
+		case mode.IsRegular():
+			files = append(files, path)
+		}
+		fmt.Printf("files: %q", files)
+		return nil
+	}
+
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
